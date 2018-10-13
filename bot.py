0df1c685c58b415278b3d712af241448d535c2de
TTPYT <edwardlanger10@gmail.com>
2018-10-13 08:54:51 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,4 @@
 import discord
-import os
-from keep_alive import keep_alive
 
 client = discord.Client()
 
@@ -14,6 +12,4 @@ async def on_message(message):
     if message.author != client.user:
         await client.send_message(message.channel, message.content[::-1])
 
-keep_alive()
-token = os.environ.get("DISCORD_BOT_SECRET")
-client.run("")
+client.login(process.env.BOT_TOKEN)
