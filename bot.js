f83718a42b649dab419872ab94479e20e3d1b559
TTPYT <edwardlanger10@gmail.com>
2018-10-31 17:27:35 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,16 +31,26 @@ client.on('message', message => {
     }
     if(spl[0] === "kick") {
      console.log("A KICK??")
-     var member= message.mentions.members.first();
-        // Kick
-        member.user.kick().then((member) => {
-            // Successmessage
-            message.channel.send(":wave: " + member.displayName + " has been successfully kicked :point_right: ");
-        }).catch(() => {
-             // Failmessage
-            message.channel.send("Access Denied");
-        });
-     console.log(member)
+     let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+     if(!kUser) return message.channel.send("Can't find user!");
+     let kReason = args.join(" ").slice(22);
+     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
+     if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
+
+     let kickEmbed = new Discord.RichEmbed()
+     .setDescription("~Kick~")
+     .setColor("#e56b00")
+     .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
+     .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
+     .addField("Kicked In", message.channel)
+     .addField("Tiime", message.createdAt)
+     .addField("Reason", kReason);
+
+     let kickChannel = message.guild.channels.find(`name`, "incidents");
+     if(!kickChannel) return message.channel.send("Can't find incidents channel.");
+
+     message.guild.member(kUser).kick(kReason);
+     kickChannel.send(kickEmbed);
     }
 });
 
