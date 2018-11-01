4157ee9d786a149ed58be67388f64df5c848f9d7
TTPYT <edwardlanger10@gmail.com>
2018-11-01 17:54:40 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,11 +2,7 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-require('discord.js-music');
- 
-// in some command
-const playlist = message.guild.playlist;
-
+let bkm = JSON.parse(fs.readFileSync("./bkm.json", "utf8"));
 
 client.on('ready', () => {
 
@@ -81,6 +77,17 @@ client.on('message', async message => {
      message.guild.member(kUser).kick(kReason);
      console.log(kUser, kReason)
      kickChannel.send({ embed });
+     if (!bkm[bUser.id]) bkm[bUser.id] = {
+        Bans: 0,
+        Kicks: 0,
+        Mutes: 0
+    };
+     let userDatas = bkm[bUser.id];
+     userDatas.kicks++;
+        fs.writeFile("./levels.json", JSON.stringify(levels), (err) => {
+        if (err) console.error(err)
+    });
+
     };
     if(spl[0] === "ban") {
      let bUser = message.guild.member(message.mentions.users.first());
@@ -133,9 +140,60 @@ client.on('message', async message => {
      message.guild.member(bUser).ban(bReason);
      incidentchannel.send({ embed });
 
-
+     if (!bkm[bUser.id]) bkm[bUser.id] = {
+        Bans: 0,
+        Kicks: 0,
+        Mutes: 0
+    };
+     let userDatas = bkm[bUser.id];
+     userDatas.bans++;
+   fs.writeFile("./bkm.json", JSON.stringify(bkm), (err) => {
+        if (err) console.error(err)
+    });
      return;
     };
+   if(spl[0]===history){
+    let hUser = message.guild.member(message.mentions.users.first());
+    let userDatas = bkm[hUser.id];
+    "You are currently level " + userDatas.level + ", with " + userDatas.points + " points."
+    const embed = {
+  "title": "~History~",
+  "description": "Oooh! Who's been naught?",
+  "color": 4903271,
+  "footer": {
+    "text": "Made by my man T1m#7219"
+  },
+  "author": {
+    "name": "Boneless Water",
+    "icon_url": "https://cdn.discordapp.com/attachments/314470306703998979/507223045136449536/52JC3VVE.png"
+  },
+  "fields": [
+    {
+      "name": "History of User",
+      "value": `${hUser} with ID ${hUser.id}`
+    },
+    {
+      "name": "Bans",
+      "value": `${userDatas.bans}`,
+      "inline": true
+    },
+    {
+      "name": "Kicks",
+      "value": `${userDatas.kicks}`,
+      "inline": true
+    },
+    {
+      "name": "Mutes",
+      "value": `${userDatas.mutes}`,
+      "inline": true
+    }
+  ]
+};
+    fs.writeFile("./bkm.json", JSON.stringify(bkm), (err) => {
+        if (err) console.error(err)
+    });
+    message.channel.send({ embed });
+   };
 });
 
  
