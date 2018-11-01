9358529ad4e4ca79f7585c63627818bd3086e92d
TTPYT <edwardlanger10@gmail.com>
2018-11-01 16:34:55 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,9 +11,9 @@ client.on('ready', () => {
 
 client.on('message', async message => { 
     if(message.author.bot) return;
-    const Testembed = new Discord.RichEmbed()
-    .setTitle("This is your title, it can hold 256 characters");
-    message.channel.send({ Testembed });
+    var embed = new Discord.RichEmbed()
+          .setTitle("This is your title, it can hold 256 characters")
+    message.channel.send({ embed });
     console.log(message.content)
     const mes = message.content.toLowerCase();
     const res = mes.substring(0, 2);
