4ceb1f8870ed6a2a8af3f455a6424c801d231fc9
TTPYT <edwardlanger10@gmail.com>
2018-11-22 17:36:04 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ pool.query('SELECT * FROM bkm', (err, res) => {
   if (err) {
     pool.query('CREATE TABLE bkm (id integer NOT NULL, bans integer NOT NULL, kicks integer NOT NULL, mutes integer NOT NULL);')
   }
-  console.log(JSON.stringify(pool.query('SELECT * FROM bkm')));
+  console.log(process.senv.DATABASE_URL));
   pool.end();
 });
 client.on('ready', () => {
